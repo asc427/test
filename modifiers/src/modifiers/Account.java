a579bf15a7091a225994914de6b3d28d9acf9c6a
asc427 <asc427@gmail.com>
2022-08-04 23:51:08 +1200

    f

---
+++ modifiers/src/modifiers/Account.java
@@ -0,0 +1,12 @@
+package modifiers;
+
+public class Account {
+	
+	public Account(String string, int i, double d) {
+	}
+	
+	public String personName;		// Name
+	public int accountNumber;		// Number
+	public static double fee;		// Monthly account fee
+	public double balance;		// Current balance
+}
